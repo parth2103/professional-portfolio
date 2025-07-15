6daf117f50c3bc819618e424a2a4bab90038fbf9
Parth Gohil <parthgohil@MacBookPro.lan>
2025-07-15 09:04:25 -0700

    Fix imports and rebuild

--- server/db.ts
+++ server/db.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-import * as schema from "./shared/schema";
+import * as schema from "./shared/schema.js";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL must be set.");
